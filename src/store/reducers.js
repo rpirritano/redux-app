38b5a9ebc6aa528fe56e8f06f722cf09d544df1e
Robert Pirritano <rpirritano@gmail.com>
2018-08-24 13:20:52 -0700

    added suggestions and fetching reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -60,3 +60,38 @@ export const allExerciseDays = (state=[], action) => {
   }
 
 }
+
+
+export const fetching = (state=false, action) => {
+
+  switch(action.type) {
+
+    case C.FETCH_RESORT_NAMES :
+      return true
+
+		case C.CANCEL_FETCHING :
+			return true
+
+		case C.CHANGE_SUGGESTIONS :
+			return false
+
+    default:
+      return state
+  }
+
+}
+
+export const suggestions = (state=[], action) => {
+
+	switch(action.type) {
+
+		case C.CLEAR_FETCHING :
+			return []
+
+		case C.CHANGE_SUGGESTIONS :
+			return action.payload
+
+		default :
+			return state
+	}
+}
