eaba79310fac065ea1229453784773839914f4ea
Robert Pirritano <rpirritano@gmail.com>
2018-08-24 16:04:21 -0700

    subscribed to the store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import initialState from './initialState.json'
 import { createStore } from 'redux'
 //createStore is used to create instances of a store
 
-const store = createStore(appReducer, initialState) //only requires the reducer. //also can take the initial state
-//so now the store holds and manages state
+//use the state from localStorage when we create the store as our initial data.
+const initalState = (localStorage['redux-store']) ?
+	JSON.parse(localStorage['redux-store']) :
+	{}
 
-console.log('inital state', store.getState()) //used to see the state in the createStore
+const store = createStore(appReducer, initalState) //only requires the reducer. //also can take the initial state
+//so now the store holds and manages state
 //the reducer is invoked once to get the initial state
+//to expose it globally use below
+window.store = store //now type store.getState() in console to access
+//console.log('inital state', store.getState()) //used to see the state in the createStore
+
+//subscribe so the state will change everytime an action is dispatch
+//store.subscribe method can be used to add callback handlers that are invoked once every time actions are dispatched
+//store.subscribe(() => console.log(store.getState()))
+
+//will also store the state in the local storage
+store.subscribe(() => {
+	const state = JSON.stringify(store.getState())
+	localStorage['redux-store'] = state
+})
+
+/*
 //dispatch is used to mutate the state and requires the action
 store.dispatch({
 	type: C.ADD_DAY,
@@ -20,4 +38,9 @@ store.dispatch({
 	}
 })
 
-console.log('next state', store.getState())
+store.dispatch({
+	type: C.SET_GOAL,
+	payload: 2
+})
+*/
+//console.log('next state', store.getState())
