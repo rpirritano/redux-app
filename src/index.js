a21c7c6f3ec9677fb5a441e34ebec22651833c2d
Robert Pirritano <rpirritano@gmail.com>
2018-08-24 19:44:37 -0700

    added more action creators

--- src/index.js
+++ src/index.js
@@ -2,20 +2,109 @@
 //They allow us to encapsulate the logic of our applications using functions, not objects
 
 import storeFactory from './store'
-import { addDay, removeDay, setGoal } from './actions'
+import {
+	addError,
+	clearError,
+	changeSuggestions,
+	clearSuggestions
+} from './actions'
 
 const store = storeFactory()
 
-const state = store.getState()
+
+
+
+
+//
+// Challenge: build addError() Action Creator
+//
 
 store.dispatch(
-	addDay("P90X", "2018-09-01")
+    addError("something went wrong")
 )
 
-store.dispatch(
-	removeDay("2018-09-01")
-)
+expect(store.getState().errors)
+  .toEqual(["something went wrong"])
+
+console.log(`
+
+    addError() Action Creator Works!!!
+
+`)
+
+
+
+
+
+
+
+
+//
+// Challenge: build clearError() Action Creator
+//
 
 store.dispatch(
-	setGoal(55)
+    clearError(0)
 )
+
+expect(store.getState().errors)
+  .toEqual([])
+
+console.log(`
+
+    clearError() Action Creator Works!!!
+
+`)
+
+
+
+
+
+
+
+
+
+
+
+//
+// Challenge: build changeSuggestios() Action Creator
+//
+
+store.dispatch(
+    changeSuggestions(['One', 'Two', 'Three'])
+)
+
+expect(store.getState().resortNames.suggestions)
+    .toEqual(['One', 'Two', 'Three'])
+
+console.log(`
+
+    changeSuggestions() Action Creator Works!!!
+
+`)
+
+
+
+
+
+
+
+
+
+
+
+//
+// Challenge: build clearSuggestions() Action Creator
+//
+
+
+
+store.dispatch(clearSuggestions())
+
+expect(store.getState().resortNames.suggestions).toEqual([])
+
+console.log(`
+
+    clearSuggestions() Action Creator Works!!!
+
+`)
